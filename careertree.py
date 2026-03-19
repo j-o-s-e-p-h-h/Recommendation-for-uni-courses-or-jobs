492f75c8452e7d53fb7e776a8b5b7be7eebbc3b3
j-o-s-e-p-h-h <josephmuleipropilot@gmail.com>
2026-03-19 10:23:53 +0200

    update careeretree.py

--- careertree.py
+++ careertree.py
@@ -6,14 +6,3 @@ class TreeNode:
 
     def add_child(self, node):
         self.children.append(node)
-
-Tech_Tree = TreeNode(None)
-medicine = TreeNode(industry_tech_data["Medicine"])
-agriculture = TreeNode(industry_tech_data["Agriculture"])
-business = TreeNode(industry_tech_data["Business"])
-
-
-Tech_Tree.add_child(medicine)
-Tech_Tree.add_child(agriculture)
-Tech_Tree.add_child(business)
-
